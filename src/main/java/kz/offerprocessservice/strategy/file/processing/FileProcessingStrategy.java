d5939322edf4217fe3a7b0175fd4341d0903f43a
Askar.A <aalyiev7@gmail.com>
2025-01-05 16:44:05 +0500

    implemented xml processing

--- src/main/java/kz/offerprocessservice/strategy/file/processing/FileProcessingStrategy.java
+++ src/main/java/kz/offerprocessservice/strategy/file/processing/FileProcessingStrategy.java
@@ -1,12 +1,14 @@
 package kz.offerprocessservice.strategy.file.processing;
 
 import kz.offerprocessservice.model.dto.PriceListItemDTO;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.Set;
 
 public interface FileProcessingStrategy {
 
-    Set<PriceListItemDTO> extract(InputStream inputStream) throws IOException;
+    Set<PriceListItemDTO> extract(InputStream inputStream) throws IOException, URISyntaxException, SAXException;
 }
--- src/main/java/kz/offerprocessservice/strategy/file/processing/impl/XmlProcessingStrategyImpl.java
+++ src/main/java/kz/offerprocessservice/strategy/file/processing/impl/XmlProcessingStrategyImpl.java
@@ -1,15 +1,48 @@
 package kz.offerprocessservice.strategy.file.processing.impl;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Unmarshaller;
 import kz.offerprocessservice.model.dto.PriceListItemDTO;
+import kz.offerprocessservice.model.xml.XmlPriceListTemplate;
+import kz.offerprocessservice.model.xml.XmlStock;
 import kz.offerprocessservice.strategy.file.processing.FileProcessingStrategy;
+import lombok.extern.slf4j.Slf4j;
+import org.xml.sax.SAXException;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class XmlProcessingStrategyImpl implements FileProcessingStrategy {
     @Override
-    public Set<PriceListItemDTO> extract(InputStream inputStream) throws IOException {
+    public Set<PriceListItemDTO> extract(InputStream inputStream) throws SAXException {
+        try {
+            JAXBContext context = JAXBContext.newInstance(XmlPriceListTemplate.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            XmlPriceListTemplate priceList = (XmlPriceListTemplate) unmarshaller.unmarshal(inputStream);
+
+            if (!priceList.getOffers().isEmpty()) {
+                return priceList.getOffers().stream()
+                        .map(offer -> {
+                            PriceListItemDTO priceListItemDTO = new PriceListItemDTO();
+                            priceListItemDTO.setOfferName(offer.getOfferName());
+                            priceListItemDTO.setOfferCode(offer.getOfferCode());
+                            Map<String, Integer> stocks = offer.getStocks().stream()
+                                    .collect(Collectors.toMap(
+                                            XmlStock::getWarehouseName,
+                                            XmlStock::getStock
+                                    ));
+                            priceListItemDTO.setStocks(stocks);
+
+                            return priceListItemDTO;
+                        }).collect(Collectors.toSet());
+            }
+        } catch (Exception e) {
+            throw new SAXException(e.getCause().toString());
+        }
+
         return Set.of();
     }
 }
