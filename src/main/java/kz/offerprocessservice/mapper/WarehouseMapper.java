22d4cb8a69a2d806a09223d4aa2a24827bd4ede9
aaliev <a.aliev@kpecom.kz>
2025-12-01 13:34:28 +0500

    test-containers

--- src/main/java/kz/offerprocessservice/mapper/WarehouseMapper.java
+++ src/main/java/kz/offerprocessservice/mapper/WarehouseMapper.java
@@ -3,10 +3,13 @@ package kz.offerprocessservice.mapper;
 import kz.offerprocessservice.model.dto.WarehouseDTO;
 import kz.offerprocessservice.model.entity.WarehouseEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface WarehouseMapper {
 
+    @Mapping(source = "merchant.id", target = "merchantId")
+    @Mapping(source = "city.id", target = "cityId")
     WarehouseDTO toDTO(WarehouseEntity entity);
 
     WarehouseEntity toEntity(WarehouseDTO dto);
