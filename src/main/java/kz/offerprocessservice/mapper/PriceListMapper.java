b080309b191d439b881a369535405bf031dbe3a2
Askar.A <aalyiev7@gmail.com>
2025-12-01 17:08:52 +0500

    fixed mapper

--- src/main/java/kz/offerprocessservice/mapper/PriceListMapper.java
+++ src/main/java/kz/offerprocessservice/mapper/PriceListMapper.java
@@ -3,11 +3,14 @@ package kz.offerprocessservice.mapper;
 import kz.offerprocessservice.model.dto.PriceListDTO;
 import kz.offerprocessservice.model.entity.PriceListEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PriceListMapper {
 
+    @Mapping(target = "merchant", ignore = true)
     PriceListEntity toEntity(PriceListDTO dto);
 
+    @Mapping(source = "merchant.id", target = "merchantId")
     PriceListDTO toDTO(PriceListEntity entity);
 }
