30302369dbb5dc57de6eb414cba319a7dd8ce867
Askar.A <aalyiev7@gmail.com>
2025-12-11 09:39:16 +0500

    added controller test

--- src/test/java/util/Fields.java
+++ src/test/java/util/Fields.java
@@ -2,6 +2,9 @@ package util;
 
 public final class Fields {
 
+    public static final String ID = "id";
+    public static final String CREATED_AT = "createdAt";
+    public static final String UPDATED_AT = "updatedAt";
     public static final String NAME = "name";
     public static final String EMAIL = "email";
 }
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,33 @@
+drop table if exists cities cascade;
+create table cities
+(
+    id         varchar(36) not null primary key,
+    name       varchar(255) not null,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+drop table if exists merchants cascade;
+create table merchants
+(
+    id         varchar(36) not null primary key,
+    name       varchar(255) not null,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+drop table if exists warehouses cascade;
+create table warehouses
+(
+    id         varchar(36) not null primary key,
+    name       varchar(255) not null,
+    merchant_id varchar(36) not null references merchants (id),
+    city_id varchar(36) not null references cities (id),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+
+
+
+
